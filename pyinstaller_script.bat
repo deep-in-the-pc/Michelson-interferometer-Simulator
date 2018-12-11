88bb53d604618c2747ce57f30f0d352fa6e24fc8
David <1160640@isep.ipp.pt>
2018-12-11 12:25:10 +0000

    Simplified pyinstaller script

--- pyinstaller_script.bat
+++ pyinstaller_script.bat
@@ -1,1 +1,8 @@
-pyinstaller main.py --clean --noconsole
+pyinstaller main.py --clean --noconsole -y
+rmdir /Q /S .\IDM\main
+xcopy .\gui .\dist\main /E
+move .\dist\main .\IDM
+rmdir /Q /S .\build
+rmdir /Q /S .\dist
+rmdir /Q /S .\__pycache__
+rm .\main.spec
