73e9b3a5aa7dbf0a4fd4ec94f775e99182a2a359
David <1160640@isep.ipp.pt>
2018-12-12 15:52:18 +0000

    Updated name of folder

--- pyinstaller_script.bat
+++ pyinstaller_script.bat
@@ -1,8 +1,8 @@
 pyinstaller main.py --clean --noconsole -y
-rmdir /Q /S .\IDM\main
+rmdir /Q /S .\SIdM\main
 mkdir .\dist\main\gui
 xcopy .\gui .\dist\main\gui /E
-move .\dist\main .\IDM
+move .\dist\main .\SIdM
 rmdir /Q /S .\build
 rmdir /Q /S .\dist
 rmdir /Q /S .\__pycache__
